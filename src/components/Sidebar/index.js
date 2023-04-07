e29d3940ec2c44e45f0aead113b80595679f8df5
Reed-Carter <reedandrew18@gmail.com>
2023-04-07 14:41:12 -0700

    displays hamburger menu when on mobile or the screen shrinks

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,38 +1,101 @@
 import './index.scss'
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser, faSuitcase } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { NavLink } from 'react-router-dom'
 
-const Sidebar = () => (
-  <div className='nav-bar'>
-    <nav>
-      <NavLink exact='true' activeclassname='active' to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink exact='true' activeclassname='active' className='portfolio-link' to="/portfolio">
-        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-      </NavLink>
-      <NavLink exact='true' activeclassname='active' className='about-link' to="/about">
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink exact='true' activeclassname='active' className='contact-link' to="/contact">
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/reed-carter/'>
-          <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
-        </a>
-      </li>
-      <li>
-        <a target='_blank' rel='noreferrer' href='https://github.com/Reed-Carter'>
-          <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
-        </a>
-      </li>
-    </ul>
-  </div>
-)
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          onClick={() => setShowNav(false)}>
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          onClick={() => setShowNav(false)}
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className='close-icon' />
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/reed-carter/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Reed-Carter"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+  )
+}
 
 export default Sidebar
