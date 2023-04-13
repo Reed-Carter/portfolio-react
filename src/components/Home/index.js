027363b0cd663d1540b0cddb36e775ec3385f1b1
Reed-Carter <reedandrew18@gmail.com>
2023-04-12 19:25:13 -0700

    add security to download link

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -50,7 +50,7 @@ const Home = () => {
         <Link to="portfolio" className='flat-button'>MY PROJECTS</Link>
         <Link to="about" className='flat-button'>ABOUT ME</Link>
         <Link to="contact" className='flat-button'>CONTACT ME</Link>
-        <Link to={{ pathname: ResumeFile }} target="_blank" className='flat-button' download>DOWNLOAD RESUME</Link>
+        <Link to={{ pathname: ResumeFile }} target="_blank" className='flat-button' rel="noopener noreferrer" download>DOWNLOAD RESUME</Link>
       </div>
       <Logo /> 
     </div>
