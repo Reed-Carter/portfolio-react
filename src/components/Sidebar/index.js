6fa67f74342dfe434de0912fdf1ccaa8e2fae633
Reed-Carter <reedandrew18@gmail.com>
2023-08-24 21:04:02 -0700

    revise resume

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import { Link } from 'react-router-dom';
-import ResumeFile from '../../assets/resume/Reed_Carter_Resume_.pdf';
+import ResumeFile from '../../assets/resume/Reed_Carter_Resume__.pdf';
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
